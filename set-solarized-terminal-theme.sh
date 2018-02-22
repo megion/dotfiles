47f642d3e4d7fa012183c38837d32ff7c9e9f34d
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2018-02-22 12:54:03 +0300

    change tmux set color script

--- set-solarized-terminal-theme.sh
+++ set-solarized-terminal-theme.sh
@@ -13,9 +13,8 @@
 #git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git 
 
 # 2. Mintty termial initial steps:
-# git clone https://github.com/mavnn/mintty-colors-solarized.git
+# git clone https://github.com/megion/mintty-colors-solarized.git 
 # cd mintty-colors-solarized 
-# chmod +x sol.dark sol.light 
 
 ############################
 
@@ -36,26 +35,14 @@ case "$OSTYPE" in
   *)        os="unknown: $OSTYPE" ;;
 esac
 
-echo "OS '$os'"
-
 if [ $os == "LINUX" ]; then
-	echo "use gnome theme"
+	#echo "use gnome theme"
     cd $gnomeThemeDir
-    #./install.sh -h
     ./install.sh -s $scheme -p $profile --skip-dircolors
 elif [ $os == "Cygwin" ]; then
-	echo "use Cygwin theme"
+	#echo "use Cygwin theme"
     cd $minttyThemeDir
-    printf '\ePtmux;\e\e]4;1;#aa0000\a\e\\'
-    if [ $scheme == "dark" ]; then
-        ./sol.dark
-    elif [ $scheme == "light" ]; then
-        ./sol.light
-    else 
-        echo "NO background"
-    fi
-
-	echo "fix tmux"
+    ./set-theme.sh $scheme
 else 
 	echo "not supported OS"
 fi
---
+++ tmux-solarized.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+############################
+#
+############################
+
+scheme=${1}
+
+tmux set -g @colors-solarized $scheme 
+~/dotfiles/set-solarized-terminal-theme.sh $scheme
+tmux source-file ~/.tmux.conf >/dev/null 2>&1
+~/dotfiles/vim-servers-theme.sh $scheme >/dev/null 2>&1
+  
+
+
+
