739f37b5da0ab3095d83b025227aaf2faed903ad
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2020-01-21 00:25:57 +0300

    add esia-auth-server.sh

--- esia-auth-server.sh
+++ esia-auth-server.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 ############################
-# util script for security-sso-esia
-# run
-# security-sso-esia.sh all
-# security-sso-esia.sh build
-# security-sso-esia.sh importCerts
-# security-sso-esia.sh deploy
-# security-sso-esia.sh run
+# util script for esia-auth-server
+# run:
+# esia-auth-server.sh all
+# esia-auth-server.sh build
+# esia-auth-server.sh importCerts
+# esia-auth-server.sh deploy
+# esia-auth-server.sh run
 ############################
 
 ########## Variables
 
 rootSrcDir=./ # or full path: ~/workspaces/rbs
 esiaOpenidAuthSrcDir=$rootSrcDir/esia-openid-auth # esia-openid-auth directory
-installDir=~/development/security-sso-esia # install directory
+installDir=~/development/esia-auth-server # install directory
 jdkDir=~/development/jdk-11.0.6 # java jdk directory
 keytool=$jdkDir/bin/keytool # keytool util
 
 keystoreDir=$installDir/keystore
 keystore=cacerts 
-buildDir=$esiaOpenidAuthSrcDir/security-sso-esia/target
+buildDir=$esiaOpenidAuthSrcDir/esia-auth-server/target
+datadbDir=/data/postgres
+
 firstParam=${1}
 
 if [ $firstParam == "build" -o $firstParam == "all" ]; then
@@ -42,15 +44,22 @@ if [ $firstParam == "importCerts" -o $firstParam == "all" ]; then
         comodorsaorganizationvalidationsecureserverca
 fi
 
+#if [ $firstParam == "initdb" -o $firstParam == "all" ]; then
+    #mkdir -p $datadbDir 
+    #COPY docker/scripts/0-init-db.sh /docker-entrypoint-initdb.d/
+    #COPY docker/scripts/1-schema-postgres.sql /docker-entrypoint-initdb.d/
+    #COPY docker/scripts/2-data-postgres.sql /docker-entrypoint-initdb.d/
+#fi
+
 if [ $firstParam == "deploy" -o $firstParam == "all" ]; then
     mkdir -p $installDir 
     mkdir -p $installDir/keystore
-    cp $buildDir/security-sso-esia-0.0.1-SNAPSHOT.war $installDir/security-sso-esia.war
+    cp $buildDir/esia-mock-server-0.0.1-SNAPSHOT.war $installDir/security-sso-esia.war
 fi
 
 if [ $firstParam == "run" -o $firstParam == "all" ]; then
     cd $installDir
-    java -Xmx1500M -Djava.security.egd=file:/dev/./urandom -jar ./security-sso-esia.war
+    java -Xmx1G -Djava.security.egd=file:/dev/./urandom -jar ./esia-mock-server.war
 fi
 
 echo "done"
