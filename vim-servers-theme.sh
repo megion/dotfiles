3e79a7c41e86bdb7f37b4edddb4000f72c7d3bdb
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2022-12-05 09:40:42 +0300

    use nvim

--- vim-servers-theme.sh
+++ vim-servers-theme.sh
@@ -15,18 +15,17 @@ scheme=${1}
 # echo "getval=${gen_fzf_default_opts}"
 # mycom=""
 
-for instance in $(vim --serverlist); do
+for instance in $(nvr --serverlist); do
     #echo "set background=$scheme for '$instance'"
     if [ $scheme == "dark" ]; then
-        vim --servername $instance --remote-send $'\e:set background=dark\n'
-        vim --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (dark)"\n'
-        vim --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33 --color info:136,prompt:136,pointer:230,marker:230,spinner:136"\n'
+        nvr --servername $instance --remote-send $'\e:set background=dark\n'
+        nvr --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (dark)"\n'
+        nvr --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33 --color info:136,prompt:136,pointer:230,marker:230,spinner:136"\n'
     elif [ $scheme == "light" ]; then
-        vim --servername $instance --remote-send $'\e:set background=light\n'
-        vim --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (light)"\n'
-        vim --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33 --color info:136,prompt:136,pointer:234,marker:234,spinner:136"\n'
+        nvr --servername $instance --remote-send $'\e:set background=light\n'
+        nvr --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (light)"\n'
+        nvr --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33 --color info:136,prompt:136,pointer:234,marker:234,spinner:136"\n'
     else 
         echo "NO background"
     fi
-
 done
