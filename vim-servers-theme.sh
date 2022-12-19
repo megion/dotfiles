30d95d63abd966eb48a5fa2e105c6aeb181c1639
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2022-12-19 09:40:42 +0300

    add solarized color

--- vim-servers-theme.sh
+++ vim-servers-theme.sh
@@ -19,10 +19,23 @@ for instance in $(vim --serverlist); do
     #echo "set background=$scheme for '$instance'"
     if [ $scheme == "dark" ]; then
         vim --servername $instance --remote-send $'\e:set background=dark\n'
+
+        vim --servername $instance --remote-send $'\e:highlight CocFloating ctermbg=8\n'
+        vim --servername $instance --remote-send $'\e:autocmd ColorScheme * hi CocFloating ctermbg=8\n'
+
+        vim --servername $instance --remote-send $'\e:highlight CocMenuSel ctermbg=0\n'
+        vim --servername $instance --remote-send $'\e:autocmd ColorScheme * hi CocMenuSel ctermbg=0\n'
+
         vim --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (dark)"\n'
         vim --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33 --color info:136,prompt:136,pointer:230,marker:230,spinner:136"\n'
     elif [ $scheme == "light" ]; then
         vim --servername $instance --remote-send $'\e:set background=light\n'
+        vim --servername $instance --remote-send $'\e:highlight CocFloating ctermbg=15\n'
+        vim --servername $instance --remote-send $'\e:autocmd ColorScheme * hi CocFloating ctermbg=15\n'
+
+        vim --servername $instance --remote-send $'\e:highlight CocMenuSel ctermbg=7\n'
+        vim --servername $instance --remote-send $'\e:autocmd ColorScheme * hi CocMenuSel ctermbg=7\n'
+
         vim --servername $instance --remote-send $'\e:let $BAT_THEME="Solarized (light)"\n'
         vim --servername $instance --remote-send $'\e:let $FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33 --color info:136,prompt:136,pointer:234,marker:234,spinner:136"\n'
     else 
