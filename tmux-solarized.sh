80b6d0e9221f4a3678553200549fe82de5ea26bc
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2023-02-14 23:50:55 +0300

    return to vim

--- tmux-solarized.sh
+++ tmux-solarized.sh
@@ -5,7 +5,10 @@
 
 scheme=${1}
 
+# echo hello 
 # echo $(nvr --serverlist)
+# echo hello2 
+# tmux -c "nvr --serverlist"
 tmux set -g @colors-solarized $scheme
 ~/dotfiles/set-solarized-terminal-theme.sh $scheme
 tmux source-file ~/.tmux.conf
